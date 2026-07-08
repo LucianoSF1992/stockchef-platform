460285be2da721c1a119687194d4db3147cdecdb
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-07-08 15:03:44 -0300

    chore: setup clean architecture project structure

--- tests/StockChef.UnitTests/obj/Debug/net9.0/StockChef.UnitTests.AssemblyInfo.cs
+++ tests/StockChef.UnitTests/obj/Debug/net9.0/StockChef.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockChef.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b84952c990a3fe3be7b0699239b2c6d07f49044")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f1c1e6b2a206363df239b7b2235f9fa8455f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockChef.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockChef.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
