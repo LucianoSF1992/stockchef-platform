9e82b534e615c058c1dc4c31ef29c5e9f81f7fc9
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-07-15 16:48:35 -0300

    feat(application): implement category commands and queries

--- src/StockChef.Application/Features/Categories/Commands/CreateCategoryCommand.cs
+++ src/StockChef.Application/Features/Categories/Commands/CreateCategoryCommand.cs
@@ -3,12 +3,6 @@ using StockChef.Application.DTOs.Categories;
 
 namespace StockChef.Application.Features.Categories.Commands;
 
-public class CreateCategoryCommand : IRequest<Guid>
-{
-    public CreateCategoryDto Category { get; set; } = null!;
-
-    public CreateCategoryCommand(CreateCategoryDto category)
-    {
-        Category = category;
-    }
-}
+public record CreateCategoryCommand(
+    CreateCategoryDto Category)
+    : IRequest<Guid>;
--- src/StockChef.Application/Features/Categories/Queries/GetAllCategoriesQuery.cs
+++ src/StockChef.Application/Features/Categories/Queries/GetAllCategoriesQuery.cs
@@ -3,6 +3,5 @@ using StockChef.Application.DTOs.Categories;
 
 namespace StockChef.Application.Features.Categories.Queries;
 
-public class GetAllCategoriesQuery : IRequest<IEnumerable<CategoryDto>>
-{
-}
+public record GetAllCategoriesQuery()
+    : IRequest<IEnumerable<CategoryDto>>;
