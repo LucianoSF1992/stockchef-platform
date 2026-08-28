8a74c5f41c14be658cdae0b1cc299d69f2813dae
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-08-27 23:06:11 -0300

    feat: create login page

--- frontend/stockchef-web/src/app/app.routes.ts
+++ frontend/stockchef-web/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
 import { MainLayout } from './layout/components/main-layout/main-layout';
 import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
 export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login,
+    },
     {
         path: '',
         component: MainLayout,
